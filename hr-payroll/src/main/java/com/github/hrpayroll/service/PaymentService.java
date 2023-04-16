69605e87222c19ae7ecfb70696ebbbb9303f176e
Lucas Montenegro <lucasvmontenegro@gmail.com>
2023-04-16 12:16:31 -0300

    feat: hrpayroll replace rest template for feign client

--- hr-payroll/src/main/java/com/github/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/github/hrpayroll/service/PaymentService.java
@@ -1,30 +1,20 @@
 package com.github.hrpayroll.service;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.github.hrpayroll.entity.Payment;
 import com.github.hrpayroll.entity.Worker;
+import com.github.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
-public class PaymentService {
-
-	@Value("${hr-worker.host}")
-	private String workerHost;
+public class PaymentService {	
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(long workerId, int days) {
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-		
-		Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+		Worker worker = workerFeignClient.findByID(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
