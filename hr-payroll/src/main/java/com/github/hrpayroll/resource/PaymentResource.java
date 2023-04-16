3beebc2e807639e3e37b18359890c747935703aa
Lucas Montenegro <lucasvmontenegro@gmail.com>
2023-04-16 15:40:50 -0300

    feat: implement hystrix fault tolerance

--- hr-payroll/src/main/java/com/github/hrpayroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/github/hrpayroll/resource/PaymentResource.java
@@ -1,5 +1,7 @@
 package com.github.hrpayroll.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +11,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.github.hrpayroll.entity.Payment;
 import com.github.hrpayroll.service.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payments")
 public class PaymentResource {
+	private static Logger logger = LoggerFactory.getLogger(PaymentResource.class);
 	
 	@Autowired
 	private PaymentService service;
 
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(long workerId, Integer days){
+		logger.warn("Alternative flow called due to worker fail");
+		Payment payment = new Payment("alternative flow", 0.0, 0);
 		return ResponseEntity.ok(payment);
 	}
 }
